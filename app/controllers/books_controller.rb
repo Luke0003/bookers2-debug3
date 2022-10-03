54ce8023954964df354e5b9ad15675c82f7d4d63
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-10-03 14:34:27 +0000

    [Finish]課題7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,18 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
     @book = Book.new
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    puts "**********************************ここから"
+    puts from..to
+    puts from...to
+    puts "**********************************"
+    @books = Book.all.sort{|a,b|
+    b.favorites.where(created_at: from...to).size <=>
+    a.favorites.where(created_at: from...to).size
+    }
+
   end
 
   def create
