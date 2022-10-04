0d15a20eb7bf5a3dd5bce3768028496d02de48c5
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-10-04 15:12:55 +0000

    [Finish]課題7b

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,11 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
+  scope :created_today, -> {where(created_at: Time.current.all_day)}
+  scope :created_yesterday, -> {where(created_at: 1.day.ago.all_day)}
+  scope :created_this_week, -> {where(created_at: 6.day.ago.at_beginning_of_day..Time.current.at_end_of_day)}
+  scope :created_last_week, -> {where(created_at: 2.week.ago.at_beginning_of_day..1.week.ago.at_end_of_day)}
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
